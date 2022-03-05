552ac4cc1592dfae69c04431aa5429c0e6f3a8ba
badcel <1218031+badcel@users.noreply.github.com>
2022-03-05 07:01:12 +0100

    Add MacOS platform support

---
+++ linux/transfer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+import shutil
+
+with open('gir_files.txt') as file:
+  gir_files = [line.rstrip() for line in file]
+
+dest_dir = os.path.abspath("./linux")
+
+for file in gir_files:
+  src = f"/usr/share/gir-1.0/{file}"
+  dest = f"{dest_dir}/{file}"
+  try:
+      shutil.copy(src, dest)
+  except FileNotFoundError:
+    print(f"Could not copy {file} to {dest_dir}.")
--- macos/transfer.py
+++ macos/transfer.py
@@ -5,10 +5,10 @@ import shutil
 with open('gir_files.txt') as file:
   gir_files = [line.rstrip() for line in file]
 
-dest_dir = os.path.abspath("./")
+dest_dir = os.path.abspath("./macos")
 
 for file in gir_files:
-  src = f"/usr/share/gir-1.0/{file}"
+  src = f"/usr/local/share/gir-1.0/{file}"
   dest = f"{dest_dir}/{file}"
   try:
       shutil.copy(src, dest)
