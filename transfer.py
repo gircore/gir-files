e59ef30888915f556983b477351468c81fc3b786
badcel <1218031+badcel@users.noreply.github.com>
2022-03-06 08:13:12 +0100

    Delete files before copying new ones

--- transfer.py
+++ transfer.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 import os
 import sys
+import glob
 import shutil
 
 source_folder = sys.argv[1]
-destination_folder = sys.argv[2]
+destination_folder = os.path.abspath(sys.argv[2])
+
+files_to_delete = glob.glob(os.path.join(destination_folder, "*"))
+for f in files_to_delete:
+    os.remove(f)
+    print(f"Deleted {f}")
 
 with open('gir_files.txt') as file:
   gir_files = [line.rstrip() for line in file]
 
 for file in gir_files:
   src = os.path.join(source_folder, file)
-  dest = os.path.join(os.path.abspath(destination_folder), file)
+  dest = os.path.join(destination_folder, file)
   try:
       shutil.copy(src, dest)
+      print(f"Copied {src} to {dest}")
   except FileNotFoundError:
     print(f"Could not copy {file} to {dest_dir}.")
     sys.exit(1)
