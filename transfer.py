8083d1252c584df9163754cdfac5187e8346df98
badcel <1218031+badcel@users.noreply.github.com>
2022-03-06 08:03:34 +0100

    Unify transfer scripts

--- transfer.py
+++ transfer.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import os
+import sys
 import shutil
+
+source_folder = sys.argv[1]
+destination_folder = sys.argv[2]
 
 with open('gir_files.txt') as file:
   gir_files = [line.rstrip() for line in file]
 
-dest_dir = os.path.abspath("./linux")
-
 for file in gir_files:
-  src = f"/usr/share/gir-1.0/{file}"
-  dest = f"{dest_dir}/{file}"
+  src = os.path.join(source_folder, file)
+  dest = os.path.join(os.path.abspath(destination_folder), file)
   try:
       shutil.copy(src, dest)
   except FileNotFoundError:
