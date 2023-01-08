94b7ebbdc18716696e5adbe75d374b3a3b5d8117
badcel <1218031+badcel@users.noreply.github.com>
2023-01-08 09:44:48 +0100

    Add WebKitGTK WebKitGTK is only support for linux

--- transfer.py
+++ transfer.py
@@ -6,16 +6,20 @@ import shutil
 
 source_folder = sys.argv[1]
 destination_folder = os.path.abspath(sys.argv[2])
+gir_list = sys.argv[3].split(',')
 
 os.makedirs(destination_folder, exist_ok=True)
 
 files_to_delete = glob.glob(os.path.join(destination_folder, "*"))
 for f in files_to_delete:
-    os.remove(f)
-    print(f"Deleted {f}")
+  os.remove(f)
+  print(f"Deleted {f}")
 
-with open('gir_files.txt') as file:
-  gir_files = [line.rstrip() for line in file]
+gir_files = []
+
+for gir in gir_list:
+  with open(gir) as file:
+    gir_files.extend([line.rstrip() for line in file])
 
 for file in gir_files:
   src = os.path.join(source_folder, file)
