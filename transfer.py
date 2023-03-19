a5cff88a21cd5fb560d67718feea02e0de31d0b9
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-03-19 23:31:36 +0100

    Query library versions

--- transfer.py
+++ transfer.py
@@ -3,6 +3,7 @@ import os
 import sys
 import glob
 import shutil
+import subprocess
 
 source_folder = sys.argv[1]
 destination_folder = os.path.abspath(sys.argv[2])
@@ -10,23 +11,24 @@ gir_list = sys.argv[3].split(',')
 
 os.makedirs(destination_folder, exist_ok=True)
 
-files_to_delete = glob.glob(os.path.join(destination_folder, "*"))
-for f in files_to_delete:
-  os.remove(f)
-  print(f"Deleted {f}")
-
 gir_files = []
 
 for gir in gir_list:
   with open(gir) as file:
-    gir_files.extend([line.rstrip() for line in file])
+    gir_files.extend([tuple(line.rstrip().split(',')) for line in file])
 
-for file in gir_files:
+for (file,pkg,maxversion) in gir_files:
   src = os.path.join(source_folder, file)
   dest = os.path.join(destination_folder, file)
-  try:
-    shutil.copy(src, dest)
-    print(f"Copied {src} to {dest}")
-  except FileNotFoundError:
-    print(f"Could not copy {src} to {dest}.")
-    sys.exit(1)
+
+  with subprocess.Popen(["pkg-config", "--exists", f"{pkg} <= {maxversion}"]) as proc:
+    proc.communicate()
+    if proc.returncode == 1:
+      print(f"Skip new version of package {pkg}")
+    else:
+      try:
+        shutil.copy(src, dest)
+        print(f"Copied {src} to {dest}")
+      except FileNotFoundError:
+        print(f"Could not copy {src} to {dest}.")
+        sys.exit(1)
